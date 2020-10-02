bdda74c5d18ec78a288a0b6264509d71836bc1ff
iavode <iavode@mail.ru>
2020-10-02 18:14:07 +0600

    Check: file reducer.py check offline.

--- hw_2/reducer.py
+++ hw_2/reducer.py
@@ -8,7 +8,7 @@ Created on Mon Sep 28 21:05:06 2020.
 from sys import stdin
 
 
-def _compute_var(*args: list):
+def _compute_var(*args):
     """Compute new var value."""
     size, current_mean, current_var, chunk_size, new_mean, new_var = args
     var = size * current_var + chunk_size * new_var
@@ -18,47 +18,48 @@ def _compute_var(*args: list):
     return var
 
 
-def _compute_mean(*args: list):
+def _compute_mean(*args):
     """Compute new mean value."""
-    chunk_size, current_mean, new_chunk_size, new_mean = args
-    mean = chunk_size * current_mean + new_chunk_size * new_mean
-    mean /= (chunk_size + new_chunk_size)
+    current_size, current_mean, new_chunk_size, new_mean = args
+    mean = current_size * current_mean + new_chunk_size * new_mean
+    mean /= (current_size + new_chunk_size)
     return mean
 
 
-def update_static_params(*args: list):
+def update_static_params(*args):
     """Update static params with new data."""
-    size, current_mean, current_var, chunk_size, new_mean, _ = args
+    current_size, current_mean, current_var, chunk_size, new_mean, _ = args
     # init params for for function _compute_var(args)
-    args_for_mean = [size, current_mean, chunk_size, new_mean]
+    args_for_mean = [current_size, current_mean, chunk_size, new_mean]
     # params for _compute_var() equal input params
-    current_mean = _compute_mean(args_for_mean)  # update mean
-    current_var = _compute_var(args)  # update var
-    size += chunk_size  # update data size
-    return size, current_mean, current_var
+    current_mean = _compute_mean(*args_for_mean)  # update current mean
+    current_var = _compute_var(*args)  # update current var
+    current_size += chunk_size  # update current data size
+    return current_size, current_mean, current_var
 
 
 def process_line(line: str):
     """Get line and process it."""
+    print(line)
     line = line.rstrip("\n")  # del \n
     line = line.split(" ")  # split input line
-    chunk_size, mean, var = tuple(map(int, line))
+    chunk_size, mean, var = tuple(map(float, line))
     return chunk_size, mean, var
 
 
 def compute_static_params():
     """
-    Compute mean and val for all dataset.
-
-    Compute mean and var in mapreduce style.
+    Compute data mean and var in mapreduce style.
     """
-    size, current_mean, current_var = 0, 0, 0  # init values for stats prams
+    size, current_mean, current_var = 0, 0, 0  # init values for static prams
     for line in stdin:
         chunk_size, new_mean, new_var = process_line(line)
-        # init params for for function compute_var(args)
+        # init params for for function update_static_params
         args = [size, current_mean, current_var, chunk_size, new_mean, new_var]
-        size, current_mean, current_var = update_static_params(args)
-    print(f"Price mean {current_mean} with var {current_var}")
+        # update static params by curent chunk values
+        size, current_mean, current_var = update_static_params(*args)
+    print(f"Price mean {current_mean}")
+    print(f"Price var {current_var}")
 
 
 if __name__ == "__main__":
