555494d8493849f84211516fb611b70aa1af8d31
iavode <iavode@mail.ru>
2020-10-02 18:00:00 +0600

    Fix: script correct work on local file. Need to check in hadoop service.

--- hw_2/mapper.py
+++ hw_2/mapper.py
@@ -5,57 +5,53 @@ Created on Mon Sep 28 21:15:03 2020.
 
 @author: Odegov Ilya
 """
-from math import isnan
 from sys import stdin
 
 
 def process_line(line) -> int:
     """Process input line."""
     # check input line type
-    try:
-        price = int(line.strip().split(",")[-7])
-        return price
-    except ValueError:
-        raise ValueError
-    except IndexError:
-        IndexError
+    price = int(line.strip().split(",")[-7])
+    return price
 
 
-def compute_static_params(mean: int, vals: list) -> tuple:
+def compute_static_params(prices_sum: int, prices_squad: list) -> tuple:
     """Compute chunk size, mean and var."""
-    size = len(vals)
+    size = len(prices_squad)
     if size:
-        mean /= size
+        mean = prices_sum / size
     else:
         return 0, 0, 0
-    var = sum(vals) / size - mean**2  # fast var computing
+    var = sum(prices_squad) / size - mean**2  # fast var computing
     return size, mean, var
 
 
-def get_lines():
+def get_line():
     """Generator for input line."""
-    with open("AB_NYC_2019.csv", "r", encoding="utf-8") as fin:
-        stdin = fin.readlines()
-        for line in stdin:
-            yield line
+    for line in stdin:
+        yield line
 
 
 def process_data():
     """Process input data."""
-    mean, prices = 0, []
-    for line in get_lines():
+    prices_sum, prices_squad = 0, []
+    for line in get_line():
         try:
             price = process_line(line)
-            mean += price
-            prices.append(price**2)
+            prices_sum += price
+            prices_squad.append(price**2)
         except ValueError:
             continue
         except IndexError:
             continue
-        except TypeError:
-            continue
-    size, mean, var = compute_static_params(mean, prices)
+    # compute chunk size, mean, var
+    size, mean, var = compute_static_params(prices_sum, prices_squad)
     print(size, mean, var)
+
+
+def generator(data):
+    for val in data:
+        yield val
 
 
 if __name__ == "__main__":
